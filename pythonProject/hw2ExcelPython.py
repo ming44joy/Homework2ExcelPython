919ed63a57dc77db5df536fa9e6f91d23614071a
ming44joy <160698697+ming44joy@users.noreply.github.com>
2024-06-11 18:08:51 -0400

    Corrected BCM excel file and appended all csv files with headers in logs folder to BCM.xlsx file

--- pythonProject/hw2ExcelPython.py
+++ pythonProject/hw2ExcelPython.py
@@ -1,33 +1,48 @@
 import pandas as pd
+import os
 from openpyxl import Workbook
+from glob import glob
 from pathlib import Path
+import shutil
 
-# Define file paths
-logs_path = Path("logs")
-BCM_file = "BMC.xlsx" # Assigning string to variable
+# Analysis 2 – Wrangling csv files
+# Get current working directory
+cwd = os.getcwd()
+print(cwd)
+print(type(cwd))
 
-# Creates a new workbook and by default the new workbook contains a single sheet
-wb = Workbook()
-wb.save(BCM_file)
+# List all files and directories in data directory
+print(os.listdir("./logs"))
 
+# Create a Path object corresponding to the new directory name
+logs_dir = Path.cwd() / 'logs'
+print(logs_dir)
 
-# Create excel writer object to write to BCM.xlsx
-with pd.ExcelWriter(BCM_file, mode="a", engine="openpyxl") as writer:
-    for file in logs_path.glob("*.csv"):
-        try:
-            # Read csv files into pandas dataframe
-            df = pd.read_csv(file)
+# Make the new directory
+logs_dir.mkdir(exist_ok=True)
+print(logs_dir.is_dir())
 
-            # Get filename to use as sheet name
-            sheet_name = file.stem
+# Find all csv files in logs directory
+csv_files = list(logs_dir.glob('*.csv'))
 
-            # Insert dataframe into excel file as new sheet
-            df.to_excel(writer, sheet_name=sheet_name, header=False, index=False)
+# Create a Pandas Excel writer using openpyxl as the engine
+BCM_file = logs_dir / 'BCM.xlsx'
+with pd.ExcelWriter(BCM_file, engine='openpyxl') as writer:
+    for csv_file in csv_files:
+        # Read each CSV file into a DataFrame
+        df = pd.read_csv(csv_file)
 
-            print(f"Inserted {file.name} into {BCM_file} as {sheet_name} sheet.")
-        except Exception as e:
-            print(f"Failed to insert {file.name}: {e}")
+        # Add headers
+        headers = ['Datetime', 'Scale', 'Temperature']
+        df.columns = headers
 
+        # Use the name of the csv file as the sheet name
+        sheet_name = csv_file.stem
+
+        # Write the DataFrame to an Excel sheet
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
+
+print(f"All CSV files have been copied to {BCM_file}")
 
 
 
