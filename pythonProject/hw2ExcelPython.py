a467241016674ba2878b74a1a554e248dee32575
ming44joy <160698697+ming44joy@users.noreply.github.com>
2024-06-11 02:26:41 -0400

    Created BCM excel file and appended all csv files in logs folder to BCM.xlsx file

--- pythonProject/hw2ExcelPython.py
+++ pythonProject/hw2ExcelPython.py
@@ -1,19 +1,43 @@
-# This imports the workbook class from openpyxl
+import pandas as pd
 from openpyxl import Workbook
-import os
+from pathlib import Path
+
+# Define file paths
+logs_path = Path("logs")
+BCM_file = "BMC.xlsx" # Assigning string to variable
 
 # Creates a new workbook and by default the new workbook contains a single sheet
 wb = Workbook()
+wb.save(BCM_file)
 
-# Save workbook
-filename = "BCM.xlsx"
-wb.save(filename)
 
-# Check if file was created
-if os.path.exists(filename):
-    print(f"File {filename} exists")
-else:
-    print(f"File {filename} does not exist")
+# Create excel writer object to write to BCM.xlsx
+with pd.ExcelWriter(BCM_file, mode="a", engine="openpyxl") as writer:
+    for file in logs_path.glob("*.csv"):
+        try:
+            # Read csv files into pandas dataframe
+            df = pd.read_csv(file)
+
+            # Get filename to use as sheet name
+            sheet_name = file.stem
+
+            # Insert dataframe into excel file as new sheet
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
+
+            print(f"Inserted {file.name} into {BCM_file} as {sheet_name} sheet.")
+        except Exception as e:
+            print(f"Failed to insert {file.name}: {e}")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
